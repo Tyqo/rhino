b32bac4a3e5789f3547aa7c790134371f06656c8
Carsten Coull <carsten.coull@swu.de>
2023-07-21 12:25:38 +0200

    publish

--- database/salutations.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `salutations` (
-  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `salutation` varchar(255) NOT NULL
-);
--- database/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `users` (
-  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `created` datetime DEFAULT NULL,
-  `modified` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-	`group_id` int DEFAULT NULL,
-	`email` varchar(255) NOT NULL,
-	`password` varchar(255) NOT NULL
-);
