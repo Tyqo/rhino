f1fbadc834ac73776f5565c9500257c231e8b826
tyqo <carsten.coull@hotmail.de>
2023-11-24 15:49:46 +0100

    lauoutMode

--- webroot/js/layout.js
+++ webroot/js/layout.js
@@ -1,18 +1,9 @@
-import LayoutElements from "/rhino/js/modules/elements.js";
-// import EditorJS from "/rhino/js/vendor/editor.js";
-
-class Layout {
-	constructor() {
-		this.debug = true;
-		window.onload = () => this.init();
-	}
-	
-	init() {
-		
-		this.elements = new LayoutElements(this);
-		// this.elements.setModal(this.LayoutModal.modal);
-	}
-}
-
-new Layout();
-//# sourceMappingURL=layout.js.map
+/**
+ * @project       tusk
+ * @author        carsten.coull@swu.de
+ * @build         Fri, Nov 24, 2023 3:49 PM ET
+ * @release       621832333ab48a5893da21f81678e52e712626a5 [main]
+ * @copyright     Copyright (c) 2023, SWU Stadtwerke Ulm / Neu-Ulm GmbH
+ *
+ */
+import LayoutElements from"/rhino/js/modules/elements.js";class Layout{constructor(){this.debug=!0,window.onload=()=>this.init()}init(){this.elements=new LayoutElements(this)}}new Layout;
--- webroot/js/modules/hooks-handler.js
+++ webroot/js/modules/hooks-handler.js
@@ -1,32 +1,9 @@
-export default class HooksHandler {
-	constructor(main) {
-		this.main = main;
-
-		if (this.main.debug) {
-			console.debug("HooksHandler::const");
-		}
-
-		// (A1) CURRENT HOOK QUEUE
-		this.queue = {};
-	}
-
-	// (A2) ADD FUNCTION TO QUEUE
-	// name : name of function to add hook to
-	// fn : function to call
-	add(name, fn) {
-		if (!this.queue[name]) { this.queue[name] = []; }
-		this.queue[name].push(fn);
-	}
-
-	// (A3) CALL A HOOK
-	// name : name of function to add hook to
-	// params : parameters
-	call(name, ...params) {
-		console.log(name);
-		if (this.queue[name]) {
-			this.queue[name].forEach((fn) => { fn(...params); });
-			delete this.queue[name];
-		}
-	}
-}
-//# sourceMappingURL=hooks-handler.js.map
+/**
+ * @project       tusk
+ * @author        carsten.coull@swu.de
+ * @build         Fri, Nov 24, 2023 3:49 PM ET
+ * @release       621832333ab48a5893da21f81678e52e712626a5 [main]
+ * @copyright     Copyright (c) 2023, SWU Stadtwerke Ulm / Neu-Ulm GmbH
+ *
+ */
+export default class HooksHandler{constructor(e){this.main=e,this.main.debug,this.queue={}}add(e,u){this.queue[e]||(this.queue[e]=[]),this.queue[e].push(u)}call(e,...u){this.queue[e]&&(this.queue[e].forEach((e=>{e(...u)})),delete this.queue[e])}}
